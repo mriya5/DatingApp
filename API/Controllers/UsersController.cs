b21db3494a6f93761ccbb7c1bee4e401fb5a1560
mriya5 <riyadwivedi67@gmail.com>
2021-11-22 17:40:34 +0530

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -20,13 +21,13 @@ namespace API.Controllers
              }
 
              [HttpGet]
-             public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+             public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
              {
                  return await _context.Users.ToListAsync();
                  
              }
              [HttpGet("{id}")]
-             public async Task<ActionResult<AppUser>> GetUsers(int id)
+             public async Task<ActionResult<AppUser>> GetUser(int id)
              {
                  return await _context.Users.FindAsync(id);
                  
--- API/Startup.cs
+++ API/Startup.cs
@@ -19,13 +19,12 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
+        
         public Startup(IConfiguration config)
         {
             _config = config;
             
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -36,6 +35,7 @@ namespace API
                  options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
              });
             services.AddControllers();
+            services.AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
@@ -52,9 +52,11 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
